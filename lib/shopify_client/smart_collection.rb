723da81faf82b1c21d69eade6ed31dc45a394d26
Esteban Pastorino <ejpastorino@gmail.com>
2013-07-22 16:12:23 -0300

    remote smart collection image hattr reader

--- lib/shopify_client/smart_collection.rb
+++ lib/shopify_client/smart_collection.rb
@@ -5,7 +5,7 @@ module ShopifyClient
 
   class SmartCollection < Base
 
-    hattr_reader :body_html, :handle, :id, :image, :products_count,
+    hattr_reader :body_html, :handle, :id, :products_count,
                  :published_at, :published_scope, :rules, :sort_order,
                  :template_suffix, :title, :updated_at
 
